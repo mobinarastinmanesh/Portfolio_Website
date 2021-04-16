28ee366c1c331a50197564cea60a4b18ff846192
mobinarastinmanesh <mobinarastinmanesh78@gmail.com>
2021-04-16 23:16:47 +0430

    netlify

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,24 +1,27 @@
-import React,{useEffect} from 'react';
+import React,{useEffect , useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import style from './ContactMe.module.css';
 import image from '../images/msg.gif';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-function Contactme() {
 
+
+
+function Contactme() {
  
   useEffect(()=>{
     AOS.init({
       duration: 2000
     })
 },[])
+   
 
   return (
-    <div  className={style.contactContaioner}  id="contact">
-      <div data-aos="fade-right" className={style.form_section , style.section} >
+    <div  className={style.contactContaioner }  id="contact">
+      <div data-aos="fade-right" className={style.form_section , style.section } >
         <h1 >Contact me &#127800;</h1>
-        <form action="/contact" name="contact" method="post">
+        <form action="/contact" name="contact" method="post"  className={ style.form}>
             <input type="hidden" name="form-name" value="contact" />
             <TextField name="name" label="Full name" />
             <TextField name="email" label="Email" />
